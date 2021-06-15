cf4383bb42332ddc2bc10bd437fd57833cf99d40
KeenNjupt <45591391+KeenNjupt@users.noreply.github.com>
2021-06-15 17:03:23 +0800

    Update cpp读入单个字符和一行字符串.cpp

--- cpp读入单个字符和一行字符串.cpp
+++ cpp读入单个字符和一行字符串.cpp
@@ -5,11 +5,15 @@ int main()
 {
     char c;
     char name[20];
+    string s1;
     cin.get(c);  //读入单个字符
     while(c!='#'){
         cout<<'\''<<c<<'\''<<',';
         cin.get(c);
     }
     cin.getline(name,20); // 读入一行字符串，包括空格
+    cout<<name<<endl;
+    getline(cin,s1); // 使用string类，读入一行字符串，包括空格
+    cout<<s1<<endl;
     return 0;
 }
