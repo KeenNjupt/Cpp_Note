82161b502f4b3d31f8f3453700e880c323e24815
KeenNjupt <45591391+KeenNjupt@users.noreply.github.com>
2021-06-15 16:58:19 +0800

    Update string类的操作.cpp

--- string类的操作.cpp
+++ string类的操作.cpp
@@ -5,12 +5,13 @@ using namespace std;
 int main()
 {
     string s1,s2;
+    int lens1;
     s1 = "keen";
     s2 = "qinqin";
-    cout<<s1<<endl;
-    cout<<s2<<endl;
+    lens1 = s1.size(); // 取字符串的长度
     s1 += s2; // 类似于整数的运算，连接s1和s2，再把字符串赋给s1
     cout<<s1<<endl;
     cout<<s2<<endl;
+    cout<<lens1<<endl;
     return 0;
 }
