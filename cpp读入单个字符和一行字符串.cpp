69545efef2026b99976d9471e4efbd6cee3df15b
KeenNjupt <45591391+KeenNjupt@users.noreply.github.com>
2021-06-15 16:20:21 +0800

    Update and rename cpp读入单个字符.c to cpp读入单个字符和一行字符串.cpp

--- cpp读入单个字符和一行字符串.cpp
+++ cpp读入单个字符和一行字符串.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main()
 {
     char c;
+    char name[20];
     cin.get(c);  //读入单个字符
     while(c!='#'){
         cout<<'\''<<c<<'\''<<',';
         cin.get(c);
     }
+    cin.getline(name,20); // 读入一行字符串，包括空格
     return 0;
 }
