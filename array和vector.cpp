a9e47d55f61d6397463e0f825505cab51ca0adf9
KeenNjupt <45591391+KeenNjupt@users.noreply.github.com>
2021-06-16 17:20:52 +0800

    Update array和vector.cpp

--- array和vector.cpp
+++ array和vector.cpp
@@ -7,10 +7,6 @@ using namespace std;
 int main(){
     array<int,5> a;// 声明一个大小为5的int数组a，类型为array，大小固定
     for(size_t i=0;i<a.size();i++) a[i] = i;
-    for(size_t i=-a.size();i<a.size();i++) 
-    {
-        cout<<"a["<<i<<"] = "<<a[i]<<endl; // 当array类型的数组下标越界时，会将下标取模
-    }
     array<int,5>a1 = a; // 可以直接拷贝
     for(size_t i=0;i<a1.size();i++) cout<<a1[i]<<endl;
     /*-----------------------------------------------*/
