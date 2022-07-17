32ec7f56b38573ef9d600ecd244b3dd0962106db
KeenNjupt <45591391+KeenNjupt@users.noreply.github.com>
2022-07-17 16:53:30 +0800

    Update and rename 数据在内存中的小端存储，即数据的高位还在地址的低位.cpp to 数据在内存中的小端存储，即数据的低位在地址的低位.cpp

--- 数据在内存中的小端存储，即数据的低位在地址的低位.cpp
+++ 数据在内存中的小端存储，即数据的低位在地址的低位.cpp
@@ -10,5 +10,7 @@ int main()
     union test t;
     t.a = 0X1A2B3C4D;
     for(int i=0;i<4;i++) cout<<hex<<uppercase<<int(t.b[i])<<endl;
+    //小端模式为：4D 3C 2B 1A， 低位地址(端)放数据低位字节(小)
+    //大端模式为：1A 2B 3C 4D   低位地址(端)放数据高位字节(大)
     return 0;
 }
